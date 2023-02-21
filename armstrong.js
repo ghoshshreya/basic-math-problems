b4008d20f73f50bcde0bc817393a0f52a0b4206f
ghoshshreya <shrghosh08@gmail.com>
2023-02-21 15:28:51 +0530

    1 file modified, 1 added

---
+++ armstrong.js
@@ -0,0 +1,8 @@
+var isArmstrong = function (num) {
+  let numArr = num.toString().split('');
+  console.log(numArr);
+};
+
+isArmstrong(123);
+
+// 1 + 8 + 27 = 36
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './style.css';
 import './count-digits.js';
 import './reverse-number.js';
 import './gcd.js';
+import './armstrong.js';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
